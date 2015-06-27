26ddd4fd9b2f6e0706a3387866ebced1cbdd382e
Dylan Allbee <dylan@linux.com>
2015-06-26 20:27:16 -0700

    Incremental update to content module
    New features:
      updatePageCache()
    
    Removed:
      getPage()

--- src/content.d
+++ src/content.d
@@ -4,77 +4,83 @@ import vibe.d;
 import app;
 //import helper.view;
 
-/**
- * Scans a directory for markdown documents
- *
- * Ignores filenames beginning with an uppercase letter. Returns a listing of
- * files found in the directory, stripping the base path and the file extension.
- *
- * Params:
- *  dir = The path (relative or absolute) to the directory to scan.
- */
-string[] getContentList(const string dir)
-{
-    import std.file: dirEntries, SpanMode;
-    import std.algorithm: filter;
-    import std.array: array;
-    import std.path: stripExtension;
-    import std.ascii: isLower;
 
-    uint pos = dir.length + 1;
-
-    return dirEntries(dir, SpanMode.depth)
-        .filter!(a => a.isFile && endsWith(a.name, ".md") && isLower(a[pos]))
-        .map!(a => stripExtension(a.name)[(pos)..$])
-        .array;
-}
 
 class ContentInterface {
+
+    private bool[string] contentHash;
 
     URLRouter register(string prefix = null)
     {
         auto router = new URLRouter(prefix);
-        router.get("/articles", &(getPage));
-        router.get("/article/:article", &(getPage));
-        router.get("/projects", &(getPage));
-        router.get("/project/:project", &(getPage));
-        router.get("/privacy", &(getPage));
-        router.get("/", &(getPage));
+        router.get("/articles", &getListing);
+        router.get("/article/:article", &getContent);
+        router.get("/projects", &getListing);
+        router.get("/project/:project", &getContent);
+        router.get("/privacy", &getPage);
+        router.get("/", &getIndex);
         import std.stdio; writeln(getContentList("content"));
 
         return router;
     }
 
-    void getPage(HTTPServerRequest req, HTTPServerResponse res)
+    /**
+     * Inserts the list of content files into the contentHash.
+     *
+     * Does not remove any outdated entries.
+     *
+     * Params:
+     *  dir = The path (relative or absolute) to the directory to scan
+     */
+    void updatePageCache(const string dir)
     {
-        //view.req = req;
-        //view.res = res;
-        /*view.page = view.loadHmap(redis.send("hgetall", "page:" ~ req.params.get("page", "")));
+        foreach(string s; getContentList(dir))
+            contentHash[s] = True;
 
-        switch (view.page.get("type", ""))
-        {
-            mixin(makeGetRender("index", "index.dt"));
-            mixin(makeGetRender("list", "list.dt"));
-            mixin(makeGetRender("article", "content.dt"));
-            default: break;
-        }*/
+        contentHash.rehash;
     }
 
-    void getIndex()
+    private void getIndex(HTTPServerRequest req, HTTPServerResponse res)
     {
         /*view.pageList = view.loadList(redis.send("zrange", "list:index", 0, 5));
         foreach(e; view.pageList)
             view.data[e] = view.loadHmap(redis.send("hgetall", "page:" ~ e));
         */
+        render!("index.dt");
     }
 
-    void getList()
+    private void getListing(HTTPServerRequest req, HTTPServerResponse res)
     {
-
+        render!("listing.dt");
     }
 
-    void getArticle()
+    private void getContent(HTTPServerRequest req, HTTPServerResponse res)
     {
+        render!("content.dt");
+    }
 
+    /**
+     * Scans a directory for markdown documents
+     *
+     * Ignores filenames beginning with an uppercase letter. Returns a listing of
+     * files found in the directory, stripping the base path and the file extension.
+     *
+     * Params:
+     *  dir = The path (relative or absolute) to the directory to scan.
+     */
+    private string[] getContentList(const string dir)
+    {
+        import std.file: dirEntries, SpanMode;
+        import std.algorithm: filter;
+        import std.array: array;
+        import std.path: stripExtension;
+        import std.ascii: isLower;
+
+        uint pos = dir.length + 1;
+
+        return dirEntries(dir, SpanMode.depth)
+            .filter!(a => a.isFile && endsWith(a.name, ".md") && isLower(a[pos]))
+            .map!(a => stripExtension(a.name)[(pos)..$])
+            .array;
     }
 }
